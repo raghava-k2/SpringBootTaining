7370962f85976e4962a4f756f3f24ec71738be4e
Vijaya Raghava Kukapalli <raghava.k2@gmail.com>
2022-11-21 10:17:51 +0530

    Modified firebase app example

---
+++ spring-boot-security-with-firebase-oauth2/src/main/java/com/gaman/FirebaseUserDTO.java
@@ -0,0 +1,15 @@
+package com.gaman;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FirebaseUserDTO {
+    private String email;
+    private String password;
+}
--- spring-boot-security-with-firebase-oauth2/src/main/java/com/gaman/FirsBaseController.java
+++ spring-boot-security-with-firebase-oauth2/src/main/java/com/gaman/FirsBaseController.java
@@ -3,11 +3,14 @@ package com.gaman;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class FirsBaseController {
 
@@ -19,8 +22,27 @@ public class FirsBaseController {
         return "public endpoint";
     }
 
+    @PostMapping("/firebase/user")
+    public FirebaseUserDTO createUser(@RequestBody FirebaseUserDTO firebaseUserDTO) throws FirebaseAuthException {
+        UserRecord.CreateRequest createRequest = new UserRecord.CreateRequest();
+        createRequest.setEmail(firebaseUserDTO.getEmail());
+        createRequest.setPassword(firebaseUserDTO.getPassword());
+        FirebaseAuth.getInstance(firebaseApp).createUser(createRequest);
+        return firebaseUserDTO;
+    }
+
     @GetMapping("/currentUser")
     public Object getCurrentLoggedInUser(@RequestParam("userId") String userId) throws FirebaseAuthException {
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        jwt.getClaims().forEach((k, v) -> {
+            log.info(" key : {} ,value  : {}", k, v);
+        });
+        jwt.getHeaders().forEach((k, v) -> {
+            log.info(" key : {} ,value  : {}", k, v);
+        });
+        jwt.getAudience().forEach(k -> {
+            log.info(" key : {} ", k);
+        });
         return FirebaseAuth.getInstance(firebaseApp).getUser(userId);
     }
 }
